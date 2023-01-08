e9997ba915ac3ebf1b8e4ce02af8d0c03999eb6f
Ömer Faruk İngeç <49835381+omer1325@users.noreply.github.com>
2023-01-08 03:03:35 +0300

    Basic OnConfiguring

---
+++ OnConfiguring/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+public class ECommerceContext : DbContext
+{
+    public DbSet<Product> Products{ get; set;}
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,1544; Database=ECommerce; User=sa; Password=1q2w3e");
+        //Provider
+        //ConnectionString
+        //Lazy Loading
+        //vb.
+    }
+}
+
+public class Product
+{
+
+}
+
+//OnConfiguring İle Konfigürasyon Ayarlarını Gerçekleştirmek
+#region Basit  Düzeyde Entity Tanımlama kuralları
+//Ef Core toool'unu yapılandırmak için kullandığımız bir metottur.
+//Context nesnesinde override edilerek kullanılmaktadır.
+#endregion
